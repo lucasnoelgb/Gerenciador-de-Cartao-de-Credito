22bdeb263494917d82d400d1cedf1c9bfb0fe311
LK <129121307+lucasnoelgb@users.noreply.github.com>
2026-09-08 10:33:34 -0300

    Estrutura inicial do projeto

---
+++ Gerenciador de Cartao/bcdados.py
@@ -0,0 +1,50 @@
+import mysql.connector
+
+def banco():
+    return mysql.connector(
+        host='localhost',
+        usuario='root',
+        senha='12345',
+        base='banco_cartao',
+    )
+
+
+
+def criar():
+    nome_fatura = "VARIAVEL"
+    valor = variavel2
+    atualizar = f'INSERT  INTO 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+conexao = banco()
+cursor = conexao.cursor()
+
+cursor.close()
+conexao.close()
+
+
+
+
+
+
---
+++ Gerenciador de Cartao/main.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta, timezone
+import bcdados
+
+brasil = timezone(timedelta(hours=-3))
+data_atual = datetime.now(brasil)
+
+# Menu inicial
+def menu():
+    print()
+
+
+
+#Conector com HTML
+#fatura total
+#fatura dos proximos meses
+#data do fechamenta da fatura
+#atualizar_cartao
+#inserir_gasto
+#deletar gasto-parcela paga
+#inserir_parcela
